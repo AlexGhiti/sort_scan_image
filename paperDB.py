0a03c27274fa839aff8aa252819e1d393c06f35e
AlexGhiti <alex.ghiti@gmail.com>
2015-12-28 23:44:18 +0100

    sort_and_scan & paperDB: New functions to deal with database.
    Here we can add vector in database looping over files in current
    directory and add some manually if the entry does not already exist.

--- paperDB.py
+++ paperDB.py
@@ -15,31 +15,61 @@ class paperDB:
         self.db_cursor = self.db_conn.cursor()
         self.dictionary = dictionary
 
+    # Table contains vector for svm learning, the filename of the image and
+    # the category in which it is stored.
     def table_create(self, tab_name):
         str_list_dict = ' INTEGER ,'.join(self.dictionary) + " INTEGER"
+        str_list_dict += ', file_name STRING'
+        str_list_dict += ', category STRING'
         sql_cmd = "CREATE TABLE IF NOT EXISTS %s (%s)" % (tab_name, str_list_dict)
-
-        print("\nCreate : " + sql_cmd)
 
         self.db_cursor.execute(sql_cmd)
         self.db_conn.commit()
 
     def table_delete(self, tab_name):
-        self.db_cursor.execute("DROP TABLE " + tab_name)
+        self.db_cursor.execute("DROP TABLE IF EXISTS " + tab_name)
         self.db_conn.commit()
 
     def table_update(tab_name, dictionary):
         return
 
+
     def table_add_column(tab_name):
         return
 
-    def table_add_vector(self, tab_name, vect):
+    # Raw accessors to database
+    def table_add_vector(self, tab_name, vect, file_name, category):
+        print("New vect ")
+        print(vect)
         str_list_value = ','.join(str(v) for v in vect)
-        sql_cmd = "INSERT INTO %s VALUES (%s)" % (tab_name, str_list_value)
+        sql_cmd = "INSERT INTO %s VALUES (%s, \"%s\", \"%s\")" % (tab_name,
+                                                                  str_list_value,
+                                                                  file_name,
+                                                                  category)
 
-        print("\nVector : " + sql_cmd)
         self.db_cursor.execute(sql_cmd)
         self.db_conn.commit()
 
         return
+
+    def table_get_all_vector(self, tab_name):
+        sql_cmd = "SELECT * FROM %s" % tab_name
+        self.db_cursor.execute(sql_cmd)
+
+        return self.db_cursor.fetchall()
+
+
+    # Smart accessors to database
+    def table_get_vector_by_name(self, tab_name, file_name):
+        sql_cmd = "SELECT * FROM %s v WHERE v.file_name = %s" % (tab_name,
+                                                                 file_name)
+        self.db_cursor.execute(sql_cmd)
+
+        return self.db_cursor.fetchall()
+
+    # List of useful query
+    def file_name_exists(self, tab_name, file_name):
+        sql_cmd = "SELECT file_name FROM %s WHERE file_name = '%s'" % (tab_name,
+                                                                file_name)
+        self.db_cursor.execute(sql_cmd)
+        return (self.db_cursor.fetchone() != None)
--- sort_scan_image.py
+++ sort_scan_image.py
@@ -16,19 +16,18 @@ def svm_init():
 # TODO Take care that some words are truncated by tokenisation
 # and bad ocr: try to fusion two consecutive words to see if
 # better results. http://blog.fouadhamdi.com/introduction-a-nltk/
+# TODO split this function in little pieces.
 def svm_sort(path, paper_db):
     vect_res = {}
 
-    with codecs.open(path, 'r', 'utf-8') as fin, codecs.open(path + '.tk', 'w', 'utf_8') as fout:
+    with codecs.open(path, 'r', 'utf-8') as fin:
         content = fin.read()
         # I could use nltk to have a better tokenisation. TODO
         list_word_with_stop = content.split()
         # Now, I need to remove the maximum of 'senseless' words and
         # remove the accents and lower useful words.
         # 1/ Start with stopword
-        list_word = [unidecode(word) for word in list_word_with_stop if word.lower() not in french_stopwords]
-        for word in list_word:
-            fout.write(word + "\n")
+        list_word = [unidecode(word.lower()) for word in list_word_with_stop if word.lower() not in french_stopwords]
         # 2/ Incoherent words from OCR.
         # TODO
         # Comparison using simhash
@@ -39,9 +38,17 @@ def svm_sort(path, paper_db):
                 if dict_word in word.lower():
                     vect_res[dict_word] += 1
 
-
-        print(vect_res)
-        paper_db.table_add_vector("test", collections.OrderedDict(sorted(vect_res.items(), key=lambda v: v[0])).values())
+        # TODO Make svm magic here
+        svm_category = "magic"
+        # Insert the vector with the category guessed by svm.
+        # Anyway, a notification will be sent to user if the
+        # category found is not good, and he will be able to
+        # if the category is wrong.
+        dict_vect = collections.OrderedDict(sorted(vect_res.items(), key=lambda v: v[0])).values()
+        if paper_db.file_name_exists("training_sample", path.split('/')[-1]) == False:
+            paper_db.table_add_vector("training_sample", dict_vect,
+                                  path.split('/')[-1],
+                                  svm_category)
 
 
 # Get french stopwords from nltk
@@ -51,12 +58,13 @@ french_stopwords = set(stopwords.words('french'))
 # utf-8 character apparently).
 with codecs.open("dictionary", 'r', 'utf-8') as fdict:
     dictionary = fdict.read().split()
-dictionary = sorted([unidecode(word) for word in dictionary])
+dictionary = sorted([unidecode(word.lower()) for word in dictionary])
 
 print("Dictionary : ")
 print(dictionary)
+
 # Path to scanned documents
-scanned_doc_path = "/home/aghiti/sample_sorting_svm/"
+scanned_doc_path = "/home/aghiti/sample_sorting_svm/sort_scan_image/"
 
 # Get all the scanned images.
 scanned_doc_list = [f for f in listdir(scanned_doc_path) if isfile(join(scanned_doc_path, f))]
@@ -65,24 +73,24 @@ for file_name in scanned_doc_list:
             scanned_doc_list.remove(file_name)
 
 paper_db = paperDB(scanned_doc_path + "test.db", dictionary)
-paper_db.table_delete("test")
-paper_db.table_create("test")
 
-svm_sort(scanned_doc_path + "scan_and_sort0.tmp.txt", paper_db) #, dictionary)
+paper_db.table_create("training_sample")
+# Get all vector from database if any
+# print(paper_db.table_get_all_vector("training_sample"))
 
-# # Launch tesseract on it.
-# for file_name in scanned_doc_list:
-#         res = 1
-#         print(file_name)
-#         #if file_name.endswith("tmp"):
-#         #    print("Converting %s" % file_name)
-#         #    res = subprocess.call(["convert", file_name, file_name + ".jpg"])
-#
-#         #res = subprocess.call(["tesseract",	scanned_doc_path + file_name,
-#         #                       scanned_doc_path + file_name,
-#         #                       "-l fra"])
-#         if (res != 0 and "txt" in file_name):
-#                 print("Tesseract on %s ok\n" % file_name)
-#                 svm_sort(scanned_doc_path + file_name)
-#         else:
-#                 print("Tesseract on %s ok\n" % file_name)
+# Launch tesseract on it.
+for file_name in scanned_doc_list:
+        res = 1
+        print(file_name)
+        #if file_name.endswith("tmp"):
+        #    print("Converting %s" % file_name)
+        #    res = subprocess.call(["convert", file_name, file_name + ".jpg"])
+
+        #res = subprocess.call(["tesseract",	scanned_doc_path + file_name,
+        #                       scanned_doc_path + file_name,
+        #                       "-l fra"])
+        if (res != 0 and "txt" in file_name):
+            print("Tesseract on %s ok\n" % file_name)
+            svm_sort(scanned_doc_path + file_name, paper_db)
+        else:
+            print("Tesseract not ok.")
